b648bbbae96e31b86a0b0d10a75d8a2865d4454a
himanshuyv <hy815088@gmail.com>
2023-08-20 19:12:06 +0530

    made homeDirectory extern

--- src/header/prompt.h
+++ src/header/prompt.h
@@ -2,6 +2,7 @@
 #define __PROMPT_H
 
 #define DIRECTORY_BUFFER_SIZE 1024
+extern char homeDirectory[DIRECTORY_BUFFER_SIZE];
 extern char absolutePath[DIRECTORY_BUFFER_SIZE];
 extern char previousDirectory[DIRECTORY_BUFFER_SIZE];
 void prompt();
